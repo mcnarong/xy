f2898673a98ead0917f7dcf088428919f441fe86
mcnarong <1586972854@qq.com>
2024-10-29 22:29:33 +0800

    Add files via upload

---
+++ lianjie.js
@@ -0,0 +1,7 @@
+var a = prompt('请输入密码：');
+        var b = 258269;
+        if (a == b) {
+            document.write('链接是：https://0b96e976-9ec3-44c0-aa2b-30bf8b0792ea.com/api/v1/client/subscribe?token=7e22d54bf813350a216f444453a7dbb4');
+        } else {
+            document.write("密码错误");
+        }
